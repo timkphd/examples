592bdf8cd98cfb9c88f2872b7587ce2cb31d3862
Timothy Kaiser <tkaiserphd@gmail.com>
2019-08-23 16:35:42 -0600

    python3 plot style

--- tims_tools/EasyDialogs.py
+++ tims_tools/EasyDialogs.py
@@ -1,10 +1,10 @@
 from sys import *
 def AskFileForOpen(message="file="):
-	fstring=raw_input(message)
+	fstring=input(message)
 	return fstring
 def AskString(message="input:",default=""):
 	prompt=message+": "+default+" "
-	fstring=raw_input(prompt)
+	fstring=input(prompt)
 	if(len(fstring)==0):
 		fstring=default
 	return fstring
