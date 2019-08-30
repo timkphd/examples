5fe04ae02f7c1e0bb1a3c77131b74b57c011a7fb
timkphd <49761448+timkphd@users.noreply.github.com>
2019-08-30 14:03:02 -0600

    file to install rcmdr

---
+++ r/ircmdr.R
@@ -0,0 +1,9 @@
+install.packages("sem", repos="http://cran.r-project.org")
+install.packages("rmarkdown", repos="http://cran.r-project.org")
+#this one fails 
+#install.packages("rgl", repos="http://cran.r-project.org")
+install.packages("multcomp", repos="http://cran.r-project.org")
+install.packages("lmtest", repos="http://cran.r-project.org")
+install.packages("leaps", repos="http://cran.r-project.org")
+install.packages("aplpack", repos="http://cran.r-project.org")
+install.packages("Rcmdr", repos="http://cran.r-project.org")
