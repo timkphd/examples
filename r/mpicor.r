6e15f81a38a5d7f102278322a58e53798000ceab
Timothy H, Kaiser, Ph.D <tkaiserphd@gmail.com>
2019-10-11 15:27:57 -0600

    getting close

--- r/mpicor.r
+++ r/mpicor.r
@@ -55,15 +55,11 @@ verbose = TRUE,
   	y=NULL
 # if y is null then we will need all of X on each node
 # since it replaces y
-    if(myid == 0 ){
-    }
   	mpi.bcast.Robj(x,comm = mpi_comm_world)  	
-    if(myid > -1 ){
-    }
   } 
   else {
 # bcast all of y to each task  
-	y<-mpi.bcast.Robj(y,comm = mpi_comm_world)
+	mpi.bcast.Robj(y,comm = mpi_comm_world)
   } 
   ## calculate remainder, largest 'size'-divisible integer and block size
   REST <- NCOL %% size
@@ -153,35 +149,95 @@ verbose = TRUE,
   ## iterate through each block combination, calculate correlation matrix
   ## between blocks and store them in the preallocated matrix on both
   ## symmetric sides of the diagonal
-  for (i in 1:nrow(COMBS)) {
-    COMB <- COMBS[i, ]    
-    G1 <- SPLIT[[COMB[1]]]
-    G2 <- SPLIT[[COMB[2]]]    
-    
-    ## if y = NULL
-    if (is.null(y)) {
-      if (verbose) cat(sprintf("#%d: %s of Block %s and Block %s (%s x %s) ... ", i, STR,  COMB[1],
-                               COMB[2], length(G1),  length(G2)))      
-      RES <- FUN(x[, G1], x[, G2], ...)
-      resMAT[G1, G2] <- RES
-      resMAT[G2, G1] <- t(RES) 
-    } else ## if y = smaller matrix or vector  
-    {
-      if (verbose) cat(sprintf("#%d: %s of Block %s and 'y' (%s x %s) ... ", i, STR,  COMB[1],
-                               length(G1),  YCOL))    
-      RES <- FUN(x[, G1], y, ...)
-      resMAT[G1, ] <- RES             
-    }
-    
-    if (verbose) {
-      timeNOW <- proc.time() - timeINIT
-      cat(timeNOW[3], "s\n")
-    }
-    
-    gc()
-  } 
   
-  return(resMAT)
+     if (myid == 0){
+		for(j in 1:(numprocs-1)) {
+		   lc<-nrow(COMBS)
+   ea<-lc/numprocs
+
+			start_block<-as.integer(j*ea+1)
+			end_block<-as.integer((j+1)*ea)
+			if(j == numprocs-1)end_block<-lc
+			section=list(1)
+			k=1
+			for ( i in start_block:end_block) {
+				COMB <- COMBS[i, ]    
+				G1 <- SPLIT[[COMB[1]]]
+				section[[k]]<-x[, G1]
+				k<-k+1
+			}
+			mpi.send.Robj(section, j, j, comm = mpi_comm_world)
+		}
+		i<-0
+		start_block<-as.integer(i*ea+1)
+		end_block<-as.integer((i+1)*ea)
+		if(j == numprocs-1)end_block<-lc
+		section=list(1)
+		k=1
+		for ( i in start_block:end_block) {
+			COMB <- COMBS[i, ]    
+			G1 <- SPLIT[[COMB[1]]]
+			section[[k]]<-x[, G1]
+			k<-k+1
+		}
+    } else {
+		stat<-0
+		section<-mpi.recv.Robj(source=0, tag=myid,comm = mpi_comm_world, status = stat)
+    }
+
+   lc<-nrow(COMBS)
+   ea<-lc/numprocs
+   start_block<-as.integer(myid*ea+1)
+   end_block<-as.integer((myid+1)*ea)
+   if(myid == numprocs-1)end_block<-lc
+   xout=list(1)
+   k=1
+	for ( i in start_block:end_block) {
+		## if y = NULL
+		if (is.null(y)) {
+			print("should not be here")
+		} else ## if y = smaller matrix or vector  
+		{
+		xout[[k]] <- FUN(section[[k]], y, ...)
+		}
+		k<-k+1
+	} 
+	
+
+	if(myid == 0){
+		resMAT <- matrix(-10.0,NCOL, YCOL)
+		lc<-nrow(COMBS)
+		ea<-lc/numprocs
+		for(j in 1:(numprocs-1)) {
+			start_block<-as.integer(j*ea+1)
+			end_block<-as.integer((j+1)*ea)
+			stat<-0
+			section<-mpi.recv.Robj(source=j, tag=j,comm = mpi_comm_world, status = stat)
+			k<-1
+			for ( i in start_block:end_block) {
+				COMB <- COMBS[i, ]    
+				G1 <- SPLIT[[COMB[1]]]
+					resMAT[G1, ]<-section[[k]]
+				k<-k+1
+				}
+			}
+		j<-0
+		start_block<-as.integer(j*ea+1)
+		end_block<-as.integer((j+1)*ea)
+		k<-1
+		for ( i in start_block:end_block) {
+			COMB <- COMBS[i, ]    
+			G1 <- SPLIT[[COMB[1]]]
+			resMAT[G1, ]<-xout[[k]]
+			k<-k+1
+			}
+		return(resMAT)
+		} else {
+		mpi.send.Robj(xout, 0, myid, comm = mpi_comm_world)
+		return(0)
+	}
+  
+  
 }
 
 
@@ -304,19 +360,7 @@ for(i in 1:dim(mymat)[1]) {
   }
 }
 nsize<-msize/2
-mat2 <- matrix(100.0,nrow=msize, ncol=nsize)
-for(i in 1:dim(mat2)[1]) {
-  for(j in 1:dim(mat2)[2]) {
-    mat2[i,j] <- 0.1
-  	if ( i == j) {
-  	  mat2[i,j] <- dia
-  	}
-  	else {
-  	  mat2[i,j] <- 0.1
-  	  mat2[i,j] <- i+1000*j
-  	}
-  }
-}
+mat2 <- matrix( rnorm(msize*nsize),nrow=msize, ncol=nsize)
 
 source("~/bin/tymer.r")
 if (!is.loaded("mpi_initialize")) {     
@@ -328,21 +372,42 @@ numprocs <<- mpi.comm.size(comm=mpi_comm_world)
 myname <- mpi.get.processor.name()
 
 if(myid == 0){
-tymer("start t1")
-t1<-cor(mymat)
-tymer("done t1")
-tymer("start t2")
-t2<-bigcor(mymat,size=block,verbose=FALSE,fun="cor")
-tymer("done t2")
-tymer("start t3")
+	tymer("start t1")
+	t1<-cor(mymat)
+	tymer("done t1")
+	tymer("start t2")
+	t2<-bigcor(mymat,size=block,verbose=FALSE,fun="cor")
+	tymer("done t2")
+	tymer("start t3")
 }
 t3<-mpicor(mymat,size=block,verbose=FALSE,fun="cor")
 if(myid == 0){
-tymer("done t3")
-print(sum(abs(t1-t2)))
-print(sum(abs(t1-t3)))
+	tymer("done t3")
+	print(sum(abs(t1-t2)))
+	print(sum(abs(t1-t3)))
+	tymer("start t4")
 }
-  print("calling finalize")
+t4<-mpicor(mymat,mymat,size=block,verbose=FALSE,fun="cor")
+if(myid == 0){
+	tymer("done t4")
+	print(sum(abs(t1-t4)))
+	tymer("start t5")
+}
+t5<-cor(mat2,mat2)
+#t5<-bigcor(mat2,mat2,size=block,verbose=FALSE,fun="cor")
+if(myid == 0){
+	tymer("done t5")
+	tymer("start t6")
+}
+# still not wokring for all cases
+t6<-mpicor(mat2,size=block,verbose=FALSE,fun="cor")
+t6<-mpicor(mat2,mat2,size=block,verbose=FALSE,fun="cor")
+if(myid == 0){
+	tymer("done t6") 
+	print(sum(abs(t5-t6)))
+}
+
+  print(c("calling finalize 0",myid))
   bonk<-mpi.finalize()
   q()  
 
