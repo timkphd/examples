f4c8fa78bb2df454b55f2be630dc49eec92349c9
Timothy H, Kaiser, Ph.D <tkaiserphd@gmail.com>
2020-03-15 11:18:27 -0600

    Added some comments and save for every loop

--- hello_bgq/ppong.c
+++ hello_bgq/ppong.c
@@ -5,16 +5,49 @@
 #include <unistd.h>
 #include <mpi.h>
 #include <math.h>
+
+// Max buffer size, actual size determined by BSIZE,
+// optionally read in from the file "BSIZE".  The
+// max size will be 4**BSIZE.  BSIZE defaluts to 15.
+// 4**15=1,073,741,824
 #define BUFSIZE 1073741824
-// #define BSIZE 15
+
+// Max times to do the send/rec loop
 #define RCOUNT 200
-#define TMAX 0.5
+
+// Number of send/rec per loop
 #define repcount 10
+
+// Max time to spend in the send/rec loop
+#define TMAX 0.5
+
+// Compile line:
+//      mpicc -DDOSAVE ppong.c -o ppong
+// If DOSAVE is defined then every loop
+// time will be recorded.
+
+// Reports MPI version
+// Nodes used for run , MPI_Wtick and timer call delta t
+// to/from processor
+// message size 
+// mintime for loop
+// avetime for loop
+// maxtime for loop
+// bandwidth
+// # of loops
+// NOTE: times are round trip not one way; so bandwidth
+// might be half of what is expected.
  
 /************************************************************
 This is a simple send/receive program in MPI
 ************************************************************/
-
+#ifdef DOSAVE
+#define FLT double
+#define INT int
+FLT **matrix(INT nrl,INT nrh,INT ncl,INT nch);
+void free_matrix(FLT **m, INT nrl, INT nrh, 
+                          INT ncl, INT nch);
+#endif
 double MYCLOCK()
 {
         struct timeval timestr;
@@ -36,6 +69,7 @@ int main(int argc,char *argv[],char *env[])
   MPI_Status status;
   double total[20],maxtime[20],mintime[20],st,et,dt;
   double t1,t2;
+  double **times;
   char fname[256];
   int count[20];
   int is,ir,mysize,isize,resultlen,repeat;
@@ -44,7 +78,10 @@ int main(int argc,char *argv[],char *env[])
   int BSIZE;
   step=4;
   FILE *file,*F;
+  char filename[32];
+  FILE *output;
   logs=log((double)step);
+
 #ifdef MPI_MAX_LIBRARY_VERSION_STRING
     char version[MPI_MAX_LIBRARY_VERSION_STRING] ;
 #else
@@ -89,6 +126,7 @@ int main(int argc,char *argv[],char *env[])
 		MPI_Recv(&t2,1,MPI_DOUBLE,ir,789,MPI_COMM_WORLD,&status);
 		printf("%d %s %e %e\n",ir,yours,t1,t2);
     }
+     printf(" S  R       Size     Min Time     Ave Time     Max Time       Bandwidth    #\n");
   } else {
         t1=MPI_Wtick();
         t2=et-st;
@@ -111,10 +149,20 @@ if(myid == -1){
 	}
 }
   tag=1234;
+#ifdef DOSAVE
+  times=matrix(0,BSIZE,1,RCOUNT);
+#endif
   for (is=0; is < numprocs-1 ; is++) {
     for (ir=is+1 ; ir < numprocs ; ir ++ ) {
       MPI_Barrier(MPI_COMM_WORLD);
       if (myid == is || myid == ir) {
+#ifdef DOSAVE
+        if(myid == is){
+          for (mysize=0;mysize <=BSIZE; mysize++)
+            for (repeat=1;repeat<=RCOUNT;repeat++)
+              times[mysize][repeat]=-1.0;              
+        }
+#endif
         for (mysize=0;mysize <=BSIZE; mysize++){
           isize=round(exp(logs*(double)mysize));
           buffer[0]=(char)0;
@@ -135,6 +183,9 @@ if(myid == -1){
                 et=TIMER();
                 dt=et-st;
                 dt=dt/repcount;
+#ifdef DOSAVE
+                times[mysize][repeat]=dt;
+#endif
                 total[mysize]=total[mysize]+dt;
                 if(dt > maxtime[mysize])maxtime[mysize]=dt;
                 if(dt < mintime[mysize])mintime[mysize]=dt;
@@ -158,9 +209,52 @@ if(myid == -1){
                   maxtime[mysize],(double)isize*2.0/mintime[mysize],count[mysize]);
           fflush(0);
         }
+#ifdef DOSAVE
+        sprintf(filename,"%4.4d_%4.4d",is,ir);
+        output=fopen(filename,"wb");
+        fwrite(&times[0][1],8,(size_t)((BSIZE+1)*RCOUNT),output);
+        fclose(output);
+#endif        
       }
     }
   }
   MPI_Finalize();
 }
 
+#ifdef DOSAVE
+FLT **matrix(INT nrl,INT nrh,INT ncl,INT nch)
+{
+    INT i;
+	FLT **m;
+	m=(FLT **) malloc((unsigned) (nrh-nrl+1)*sizeof(FLT*));
+	if (!m){
+	     printf("allocation failure 1 in matrix()\n");
+	     exit(1);
+	}
+	m -= nrl;
+	for(i=nrl;i<=nrh;i++) {
+	    if(i == nrl){ 
+		    m[i]=(FLT *) malloc((unsigned) (nrh-nrl+1)*(nch-ncl+1)*sizeof(FLT));
+		    if (!m[i]){
+		         printf("allocation failure 2 in matrix()\n");
+		         exit(1);
+		    }		
+		    m[i] -= ncl;
+	    }
+	    else {
+	        m[i]=m[i-1]+(nch-ncl+1);
+	    }
+	}
+	return m;
+}
+
+void free_matrix(FLT **m, INT nrl, INT nrh, 
+                          INT ncl, INT nch) 
+/* free a float matrix allocated by matrix() */
+{
+  m[nrl] += ncl;
+  free((char*) (m[nrl]));
+  m += nrl; 
+  free((char*) (m)); 
+}
+#endif
