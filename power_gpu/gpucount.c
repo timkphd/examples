ff08d0561749fb268821fc0d56df129128d03a01
Timothy H, Kaiser, Ph.D <tkaiserphd@gmail.com>
2020-04-15 09:11:54 -0600

    gpucount.c update

--- power_gpu/gpucount.c
+++ power_gpu/gpucount.c
@@ -1,8 +1,17 @@
 #include <cuda.h>
 #include <stdlib.h>
 #include <stdio.h>
-main() {
+int cudaGetDeviceCount(int *i);
+int cudaRuntimeGetVersion(int *i);
+int cudaDriverGetVersion(int *i);
+void main() {
 	int num_devices;
+        int rt,dr;
 	cudaGetDeviceCount(&num_devices);
 	printf("%d",num_devices);
+        if(num_devices > 0) {
+         cudaRuntimeGetVersion(&rt);
+         cudaDriverGetVersion(&dr);
+         printf("\nRuntimeVersion= %d  DriverVersion= %d\n",rt,dr);
+        }
 }
