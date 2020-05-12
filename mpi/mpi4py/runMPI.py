c7204aa1452e01d68a4901142b6f49c10da8aeb3
Tim Kaiser <tkaiser2@nrel.gov>
2020-05-12 13:44:55 -0600

    clean up runMPI.py

--- mpi/mpi4py/runMPI.py
+++ mpi/mpi4py/runMPI.py
@@ -2,43 +2,23 @@
 # coding: utf-8
 ##### Generated via
 ##### jupyter nbconvert --to script runMPI.ipynb 
-# In[6]:
-
-
+# In[0]:
 import sys
 import os
 
-
-# In[7]:
-
-
+# In[1]:
 doit=os.popen("srun -n 4 ./P_ex02.py")
 output=doit.readlines()
 
-
-# In[8]:
-
-
+# In[2]:
 for o in output:
     print(o.strip())
 
-
-# In[4]:
-
-
+# In[3]:
+# Assumes this program has been built
 doit=os.popen("srun -n 4 ../c_ex02")
 output=doit.readlines()
 
-
 # In[9]:
-
-
 for o in output:
     print(o.strip())
-
-
-# In[ ]:
-
-
-
-
