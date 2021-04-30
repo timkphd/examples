0de9280c82bf027dd59af872e92af2f86947202f
Timothy Kaiser <tkaiserphd@gmail.com>
2021-04-29 20:34:44 -0600

    flower.R

--- r/flower.R
+++ r/flower.R
@@ -9,25 +9,15 @@ tag<-1234;
 source<-0;
 destination<-1;
 count<-1;
-paste("R am",myid,"of",numprocs,"on",myname)
-bots<-integer(numprocs)
-tops<-integer(numprocs)
+cat("I am",myid,"of",numprocs,"on",myname,"\n")
 if (myid == 0 ) {
   library(datasets)
-  set1<-iris[iris$Species == "setosa", ]
   set2<-iris[iris$Species == "versicolor", ]
-  #set3<-iris[iris$Species == "virginica", ]
-  each=as.integer(nrow(set1)/numprocs)
-  for (n in 1:numprocs) {
-    bots[n]<-(n-1)*each+1
-    tops[n]<-bots[n]+each-1
-  }
-  tops[numprocs]=nrow(set1)
-  print(bots)
-  print(tops)
-  myset<-set1[bots[1]:tops[1],]
 } else {
-set2<-integer(1)
+# For the bcast we need to set input 
+# to something on every processor
+# even though technically it is not used.
+  set2<-integer(1)
 }
 Sys.sleep(2)
 set2<-mpi.bcast.Robj(set2,0,comm=mpi_comm_world)
@@ -36,6 +26,20 @@ if (myid == 1) {
 }
 Sys.sleep(2)
 if (myid == 0 ) {
+  set1<-iris[iris$Species == "setosa", ]
+  each=as.integer(nrow(set1)/numprocs)
+# We are going to send a section of "setosa"
+# to each processor. This gives the ranges.
+  bots<-integer(numprocs)
+  tops<-integer(numprocs)
+  for (n in 1:numprocs) {
+    bots[n]<-(n-1)*each+1
+    tops[n]<-bots[n]+each-1
+  }
+  tops[numprocs]=nrow(set1)
+  cat("bots",bots,"\n")
+  cat("tops",tops,"\n")
+  myset<-set1[bots[1]:tops[1],]
   for (n in 1:(numprocs-1)) {
     tosend<-set1[bots[n+1]:tops[n+1],]
     mpi.send.Robj(tosend,n,tag=1234,comm=mpi_comm_world)
@@ -48,7 +52,7 @@ if (myid == 0 ) {
 #Sepal.Length Sepal.Width Petal.Length Petal.Width
 sizes=c(sum(myset[["Sepal.Length"]]),mean(myset[["Sepal.Width"]]),
         sum(myset[["Petal.Length"]]),mean(myset[["Petal.Width"]]))
-cat(myid,sizes,"\n")
+cat(myid,"sums",sizes,"\n")
 thetot<-mpi.reduce(sizes, type=2, op="sum",dest = 0, comm = mpi_comm_world)
 if(myid == 0) {
   thetot=thetot/nrow(set1)
