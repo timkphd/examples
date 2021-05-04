0fd1b049f022336c2dd205d48fd4697d30e9551e
Timothy Kaiser <tkaiserphd@gmail.com>
2021-05-04 13:32:31 -0600

    chmod

---
+++ r/bind.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+# start the cluster
+library(foreach)
+library(doParallel)
+numprocs=parallel::detectCores()
+cl <- makeCluster(numprocs)
+registerDoParallel(cl)
+nt<-900
+
+# default is a list
+aset <- foreach (ijk=1:nt) %dopar% {
+    aset<-c(ijk,Sys.getpid())
+}
+
+
+head(aset)
+
+
+aset <- foreach (ijk=1:nt , .combine=rbind) %dopar% {
+    aset<-c(ijk,Sys.getpid())
+}
+
+
+head(aset)
+
+aset <- foreach (ijk=1:nt , .combine=cbind) %dopar% {
+    aset<-c(ijk,Sys.getpid())
+}
+
+head(aset)
