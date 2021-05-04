7b92d8eef4ea8db1048d2a3f966aa53f71cf7017
Timothy Kaiser <tkaiserphd@gmail.com>
2021-05-04 09:49:53 -0600

    r/hellop.R output

--- r/hellop.R
+++ r/hellop.R
@@ -7,12 +7,12 @@ cl <- makeCluster(numprocs)
 registerDoParallel(cl)
 
 # do something
-nt<-900
+nt<-90
 aset <- foreach (ijk=1:nt) %dopar% {
     aset<-c(ijk,Sys.getpid())
 }
 # stop the cluster
-stopCluster(cluster)
+stopCluster(cl)
 
 # use the results
 df<-data.frame(iter=integer(),pid=integer())
@@ -23,10 +23,12 @@ for (ijk in 1:nresults){
     }
 
 str(df)
+df2<-data.frame(pid=integer(),its=integer(),load=double())
 pids=unique(df['pid'])
 for (p in pids[,]) {
     n<-nrow(df[df['pid'] == p,])
-    load<-n/nt
-    cat(n,load,"\n")
+    load<-100.0*(n/nt)
+    df2[nrow(df2)+1,]<-list(p,n,load)
 }
-
+df2<-df2[order(df2$pid),]
+print(df2)
