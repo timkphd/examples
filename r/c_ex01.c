a402628f398cf1de2ff6ed1ffd69af0d2b6bc814
Timothy Kaiser <tkaiserphd@gmail.com>
2021-05-04 08:30:59 -0600

    add processor name to c_ex01.c

--- r/c_ex01.c
+++ r/c_ex01.c
@@ -7,18 +7,19 @@
 This is a simple send/receive program in MPI
 ************************************************************/
 
-int main(argc,argv)
-int argc;
-char *argv[];
+int main(int argc,char *argv[])
 {
     int myid, numprocs;
-    int tag,source,destination,count;
+    int tag,source,destination,count,resultlen;
     int buffer;
     MPI_Status status;
+    char myname[MPI_MAX_PROCESSOR_NAME] ;
  
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+    MPI_Get_processor_name(myname,&resultlen);
+    printf("C Hello from %s %d %d\n",myname,myid,numprocs);
     tag=1234;
     source=0;
     destination=1;
