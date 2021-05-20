9bbd6addbb487db1f2f6115f2546ba41cf0c6bf5
Timothy Kaiser <tkaiserphd@gmail.com>
2021-05-20 11:16:41 -0600

    added vector version of P_ex01c.py f_ex01c.f90

--- mpi/mpi4py/P_ex01b.py
+++ mpi/mpi4py/P_ex01b.py
@@ -6,6 +6,8 @@ from numpy import *
 # mpi4py module
 from mpi4py import MPI
 
+def utf8len(s):
+    return len(s.encode('utf-8'))
 
 # Initialize MPI and print out hello
 comm=MPI.COMM_WORLD
@@ -19,26 +21,34 @@ mytag=1234
 # Process 0 is going to send the data
 mysource=0
 
-# Process 1 is going to send the data
+# Process 1 is going to get the data
 mydestination=1
 
 # Sending a single value each time
-count=1
+count=18
+#count=5
+gcount=count
+print(utf8len('hello'))
 # Do the send and recv a number of times
 for k in range(1,4):
 	if myid == mysource:
 # For the upper case calls we need to send/recv numpy arrays	
-		buffer=array(['hello '+str(k)], dtype=str)
+		#buffer=array(['hello'], dtype=str)
+		buffer=array('hello', dtype=str)
+		#buffer='hello'
 # We are sending a string, count is optional, to mydestination	
 		comm.Send([buffer,count,MPI.CHAR], dest=mydestination, tag=mytag)
+		#comm.Send(buffer, dest=mydestination, tag=mytag)
 		print("Python processor ",myid," sent ",buffer)
 
 	if myid == mydestination:
-		if(k == 1) : buffer=array((1), dtype=str)
+		#if(k == 1) : buffer=array(['xxxxx'], dtype=str)
+		if(k == 1) : buffer=array('xxxxx', dtype=str)
 # We are receiving an string, size is optional, from mysource	
 # For the upper case versions of routines data is returned in a buffer
 # Not as the return value for the function
-		comm.Recv([buffer,MPI.CHAR], source=mysource, tag=mytag)
+		comm.Recv([buffer,gcount,MPI.CHAR], source=mysource, tag=mytag)
+		#comm.Recv([buffer], source=mysource, tag=mytag)
 		print("Python processor ",myid," got ",buffer)
 
 MPI.Finalize()
--- mpi/mpi4py/P_ex01c.py
+++ mpi/mpi4py/P_ex01c.py
@@ -23,12 +23,14 @@ mysource=0
 mydestination=1
 
 # Sending a single value each time
-count=1
+count=4
 # Do the send and recv a number of times
 for k in range(1,4):
 	if myid == mysource:
 # For the upper case calls we need to send/recv numpy arrays	
-		buffer=array(k+5678,"i")
+		buffer=empty((count),"i")
+		for ic in range(0,count):
+			buffer[ic]=5678+1000*(ic+1)
 # We are sending a integer, count is optional, to mydestination	
 		comm.Send([buffer, count,MPI.INT],dest=mydestination, tag=mytag)
 		print("Python processor ",myid," sent ",buffer)
@@ -37,7 +39,7 @@ for k in range(1,4):
 # We are receiving an integer, size is optional, from mysource	
 # For the upper case versions of routines data is returned in a buffer
 # Not as the return value for the function
-		if(k == 1) : buffer=empty((1),"i")
+		if(k == 1) : buffer=empty((count),"i")
 		comm.Recv([buffer, MPI.INT], source=mysource, tag=mytag)
 		print("Python processor ",myid," got ",buffer)
 
