0245102804d535bd1c9dff2f419611421ae1beb3
Tim Kaiser <tkaiserphd@gmail.com>
2021-09-13 16:13:27 -0600

    Rjupyter.R

---
+++ r/Rjupyter.R
@@ -0,0 +1,4 @@
+install.packages(c("repr", "IRdisplay", "evaluate", "crayon", "pbdZMQ", "devtools", "uuid", "digest"),repos='http://cran.us.r-project.org')
+install.packages("IRkernel",repos='http://cran.us.r-project.org')
+IRkernel::installspec()
+
