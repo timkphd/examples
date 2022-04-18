d3c89789a4d5605a5303d9d8fe2017978a0a49bb
Timothy H, Kaiser, Ph.D <tkaiserphd@gmail.com>
2022-04-18 11:06:23 -0600

    exchange.c

--- mpi/exchange.c
+++ mpi/exchange.c
@@ -9,6 +9,10 @@ int main(int argc,char *argv[])
 /* Variables we are going to pass around */
   int value;
   int sendl,sendr,recvl,recvr;
+  int iterations,iter;
+  int res,gres;
+  
+/
   
 /* Some variables required by MPI */
   MPI_Status status; 
@@ -39,7 +43,13 @@ sendl= 100+myid;
 sendr= 200+myid; 
 recvl=-1000;
 recvr=-1000;
+/* Zeroth processor broadcasts # iterations to all the rest */
+    int root=0;
+    if (myid == root) iterations=3;
+    MPI_Bcast(&iterations,   1,MPI_INT,   root,MPI_COMM_WORLD);
+    printf("proc %d got %6d %6d with bcast value %6d\n",myid,recvl,recvr,value); 
 
+for (iter=0 ;iter< iterations;iter++) {
   if((myid % 2) == 0){
 /* send to left */
       MPI_Send(&sendl,1,MPI_INT,left,10, MPI_COMM_WORLD);
@@ -50,7 +60,7 @@ recvr=-1000;
 /* send to right */
       MPI_Send(&sendr,1,MPI_INT,right,10, MPI_COMM_WORLD);
     }
-    else{
+  else{
 /* rec from right */
       MPI_Recv(&recvr,1,MPI_INT,right,10,MPI_COMM_WORLD,&status);
 /* send to right */
@@ -61,11 +71,10 @@ recvr=-1000;
       MPI_Recv(&recvl,1,MPI_INT,left,10,MPI_COMM_WORLD,&status);
     }
 
-/* Last processor broadcasts it to all the rest */
-    int root=numprocs-1;
-    value=myid;
-    MPI_Bcast(&value,   1,MPI_INT,   root,MPI_COMM_WORLD);
-    printf("proc %d got %6d %6d with bcast value %6d\n",myid,recvl,recvr,value); 
+  res=recvl+recvr;
+  MPI_Allreduce(&res, &gres, 1,MPI_INT MPI_SUM, MPI_COMM_WORLD);
+  printf("%d %d\n",myid,gres);
+}
 /*  Stop MPI */  
   int ierr=MPI_Finalize();
   return(ierr);
