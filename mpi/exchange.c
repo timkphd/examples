74134b0cd59d25217c6202b6630dfa6f03b2cf87
Timothy H, Kaiser, Ph.D <tkaiserphd@gmail.com>
2022-04-18 11:41:01 -0600

    exchange.c

--- mpi/exchange.c
+++ mpi/exchange.c
@@ -74,7 +74,7 @@ for (iter=0 ;iter< iterations;iter++) {
   res=recvl+recvr+iter;
   sendl=sendl+recvr;
   sendr=sendr+recvl;
-  value=sendl+sendr;
+  value=value+sendl+sendr;
   
 /* and a reduction to see what's happening */
   MPI_Allreduce(&res, &gres, 1,MPI_INT, MPI_SUM, MPI_COMM_WORLD);
