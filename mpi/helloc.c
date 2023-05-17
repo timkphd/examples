9c9d17893a1981df156d07e9b6b4f8b7a6aafd1e
Tim Kaiser <tkaiser2@nrel.gov>
2023-05-17 08:35:26 -0600

    added version to hello

--- mpi/helloc.c
+++ mpi/helloc.c
@@ -10,12 +10,18 @@ name, rank, and total run size.
 int main(int argc, char **argv)
 {
     int myid,numprocs,resultlen;
+    char version[MPI_MAX_LIBRARY_VERSION_STRING];
     char myname[MPI_MAX_PROCESSOR_NAME] ;
+    int vlan;
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
     MPI_Get_processor_name(myname,&resultlen); 
     printf("Hello from %s %d %d\n",myname,myid,numprocs);
+    if (myid == 0 ) {
+	    MPI_Get_library_version(version, &vlan);
+	    printf("%s\n",version);
+    }
     MPI_Finalize();
 }
 
--- mpi/hellof.f90
+++ mpi/hellof.f90
@@ -6,12 +6,17 @@
       program hello
       include "mpif.h"
       integer myid,numprocs,ierr,nlength
-      character (len=MPI_MAX_PROCESSOR_NAME):: myname
+      character(len=MPI_MAX_LIBRARY_VERSION_STRING+1) :: version
+      character (len=MPI_MAX_PROCESSOR_NAME+1):: myname
       call MPI_INIT( ierr )
       call MPI_COMM_RANK( MPI_COMM_WORLD, myid, ierr )
       call MPI_COMM_SIZE( MPI_COMM_WORLD, numprocs, ierr )
       call MPI_Get_processor_name(myname,nlength,ierr)
+      call MPI_Get_library_version(version, nlength, ierr)
       write (*,*) "Hello from ",trim(myname)," # ",myid," of ",numprocs
+      if (myid .eq. 0)then
+              write(*,*)trim(version)
+      endif
       call MPI_FINALIZE(ierr)
       stop
       end
