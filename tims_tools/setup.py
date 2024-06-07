6cda331b89c7741decbc78867961cebc057186f4
Timothy Kaiser <tkaiser2@kl1.head.cm.kestrel.hpc.nrel.gov>
2024-06-07 11:20:31 -0600

    setup.py for tymer.py

---
+++ tims_tools/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup, Extension
+
+setup (name = 'tymer',
+       version = '1.0',
+       description = 'This is a utililty module with a tymer',
+       py_modules = ['tymer'])
+
+
+##python setup.py install
+
