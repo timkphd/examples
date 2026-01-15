106193ba4c7f38e4cd6a9a0fa4e2e6165c5b3c26
Tim Kaiser <tkaiserphd@gmail.com>
2026-01-15 13:46:22 -0600

    heatmap.jl

---
+++ julia/heatmap.jl
@@ -0,0 +1,36 @@
+using Plots
+
+f = (x, y) -> x^2 + y^2
+
+x = range(-1, 1; length=100)
+y = range(-2, 2; length=400)
+z = fill(NaN, size(y, 1), size(x, 1))
+
+for i in eachindex(x), j in eachindex(y)
+    z[j, i] = f(x[i], y[j])
+end
+heatmap(x, y, z)
+
+function read_2d_binary(filename::String, rows::Int, cols::Int, dtype::Type)
+    # Create an uninitialized 2D array (Matrix) of the specified type and dimensions.
+    # Julia's column-major memory layout naturally handles sequential binary reads.
+    data = Matrix{dtype}(undef, rows, cols)
+
+    # Open the file in read mode ("r") using a do block for automatic closing
+    open(filename, "r") do io
+        # Read the binary data from the stream 'io' and fill the 'data' array
+        read!(io, data)
+    end
+
+    return data
+end
+
+dat=read_2d_binary("/Users/tkaiser2/web/examples/stommel/stom.dat",202,202,Float64)
+
+
+x = range(-1, 1; length=202)
+y = range(-1, 1; length=202)
+dat=dat/500000
+heatmap(x, y, dat)
+
+
